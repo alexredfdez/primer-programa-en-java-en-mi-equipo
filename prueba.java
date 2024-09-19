50d63fa21630302bb26231b38c1ba0e69ea387bd
Alejandro Redondo Fernández <alejandroredondo@MacBook-Air-de-Alex.local>
2024-09-19 09:54:17 +0200

    Comida favorita

--- prueba.java
+++ prueba.java
@@ -3,5 +3,8 @@ public class prueba {
     public static void main(String args[]) {
         System.out.println("Redondo Fernández");
         System.out.println("20 2004");
+        System.out.println("Pizza");
+
     }
+    
 }
