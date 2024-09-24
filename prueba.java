05e9047d3f80fb576e34a951eb5dbf88210c9530
Alejandro Redondo Fernández <alejandroredondo@MacBook-Air-de-Alex.local>
2024-09-24 09:37:03 +0200

    Cantante favorito

--- prueba.java
+++ prueba.java
@@ -1,9 +1,9 @@
 public class prueba {
 
     public static void main(String args[]) {
-        System.out.println("Redondo Fernández");
         System.out.println("20 2004");
         System.out.println("Pizza");
+        System.out.println("Eladio Carrión");
 
     }
     
